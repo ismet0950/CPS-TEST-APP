3fbc6dcfa526057dfd3f41dc7def8375c7a99ed4
ismet0950 <mamedovisi57@gmail.com>
2026-02-22 23:00:37 +0300

    CPS-TEST v2.0.0

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -7,6 +7,7 @@ const Button = () => {
     const [time, setTime] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
     const [result, setResult] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
 
     useEffect(() => {
 
@@ -28,7 +29,7 @@ const Button = () => {
         const score = buttonValue;
         setResult(score);
         console.log("Final Score:", score);
-
+        setGameOver(false);
         setIsRunning(false);
         setTime(0);
         setButtonValue(0);
@@ -37,26 +38,25 @@ const Button = () => {
     return (
         <div className='main-container'>
             <div className="data-label">
-                <div className="score-panel"><span>Score</span><span className='m-5 text-white-500'>{buttonValue}</span></div>
-                <div className="time-panel"><span>Time</span><span className='m-5 text-red-500'>{time}</span></div>
-                <div className="result-panel"><span>Result</span><span className='m-5 text-red-500'>{result}</span></div>
+                <div className="score-panel"><span>Score</span><span className='score-value m-5 text-white-500'>{buttonValue}</span></div>
+                <div className="time-panel"><span>Time</span><span className='score-value m-5 text-red-500'>{time}</span></div>
+                <div className="result-panel"><span>Result</span><span className='score-value m-5 text-red-500'>{result}</span></div>
                 <button
-                    onClick={() => {
-                        setButtonValue(buttonValue + 1);
-                        setIsRunning(true)
-                    }}
+                    onClick={() => setGameOver(true)}
                     className="m-4 p-5 retry-button border-none"
                 >RETRY</button>
             </div>
-            <div className="button-section">
-                <button
-                    onClick={() => {
-                        setButtonValue(buttonValue + 1);
-                        setIsRunning(true)
-                    }}
-                    className="m-4 p-5 score-button border-none"
-                >CLICK</button>
-            </div>
+            {
+                gameOver && <div className="button-section">
+                    <button
+                        onClick={() => {
+                            setButtonValue(buttonValue + 1);
+                            setIsRunning(true)
+                        }}
+                        className="m-4 p-5 score-button border-none"
+                    >CLICK</button>
+                </div>
+            }
         </div >
     );
 };
